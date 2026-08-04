651d11ed3bfe7197890199a924343f5e198f9e94
Fateme Hosseinmardi <fateme.hosseinmardi@gmail.com>
2026-08-04 20:26:42 +0200

    Configure SQL migration retries

--- api/TaskDbContextFactory.cs
+++ api/TaskDbContextFactory.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using System.Text.Json;
 
 namespace TaskTrackerFunctions;
 
@@ -9,12 +10,29 @@ public sealed class TaskDbContextFactory : IDesignTimeDbContextFactory<TaskDbCon
     public TaskDbContext CreateDbContext(string[] args)
     {
         var connectionString = Environment.GetEnvironmentVariable("TASKS_SQL_CONNECTION_STRING")
+            ?? ReadLocalSqlConnectionString()
             ?? "Server=localhost;Database=TaskTrackerDesignTime;Trusted_Connection=True;TrustServerCertificate=True";
 
         var options = new DbContextOptionsBuilder<TaskDbContext>()
-            .UseSqlServer(connectionString)
+            .UseSqlServer(connectionString, sqlOptions => sqlOptions.EnableRetryOnFailure())
             .Options;
 
         return new TaskDbContext(options);
+    }
+
+    private static string? ReadLocalSqlConnectionString()
+    {
+        var localSettingsPath = Path.Combine(Directory.GetCurrentDirectory(), "local.settings.json");
+        if (!File.Exists(localSettingsPath))
+        {
+            return null;
+        }
+
+        using var document = JsonDocument.Parse(File.ReadAllText(localSettingsPath));
+
+        return document.RootElement
+            .GetProperty("Values")
+            .GetProperty("TASKS_SQL_CONNECTION_STRING")
+            .GetString();
     }
 }
